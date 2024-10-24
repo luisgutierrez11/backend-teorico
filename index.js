32196446c7add1de9aa1331b9c783d3ee23e2224
luisgutierrez11 <luis.gut.11jm@gmail.com>
2024-10-23 23:19:57 -0300

    asd

--- index.js
+++ index.js
@@ -12,17 +12,17 @@ let notes = [
     id: 1,
     content: "HTML is easy",
     important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only JavaScript",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true
-    }
+  },
+  {
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    important: false
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
+  }     
   ]
   
 // const requestLogger = (request, response, next) => {
@@ -100,6 +100,3 @@ const PORT = 3001
 app.listen(PORT, () => {
 console.log(`Server running on port ${PORT}`)
 })
-
-
-// asd
